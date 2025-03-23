4af0ff92cad5fe25cb5b0c22b4f9d5c554208be5
ekojima <tsujigamieri@gmail.com>
2025-03-23 17:08:23 +0900

    Norm修正済：check_constraints + is_valid

--- ex00/check_constraints.c
+++ ex00/check_constraints.c
@@ -6,7 +6,7 @@
 /*   By: ekojima <ekojima@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 16:10:36 by ekojima           #+#    #+#             */
-/*   Updated: 2025/03/23 16:11:12 by ekojima          ###   ########.fr       */
+/*   Updated: 2025/03/23 16:59:26 by ekojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	visible_count(int a, int b, int c, int d)
 	return (count);
 }
 
-int	check_constraints(int grid[4][4], int constraints[16])
+int	check_vertical(int grid[4][4], int constraints[16])
 {
 	int	i;
 
@@ -45,31 +45,34 @@ int	check_constraints(int grid[4][4], int constraints[16])
 		if (visible_count(grid[0][i], grid[1][i], grid[2][i],
 				grid[3][i]) != constraints[i])
 			return (0);
-		i++;
-	}
-	i = 0;
-	while (i < 4)
-	{
 		if (visible_count(grid[3][i], grid[2][i], grid[1][i],
 				grid[0][i]) != constraints[4 + i])
 			return (0);
 		i++;
 	}
+	return (1);
+}
+
+int	check_horizontal(int grid[4][4], int constraints[16])
+{
+	int	i;
+
 	i = 0;
 	while (i < 4)
 	{
 		if (visible_count(grid[i][0], grid[i][1], grid[i][2],
 				grid[i][3]) != constraints[8 + i])
 			return (0);
-		i++;
-	}
-	i = 0;
-	while (i < 4)
-	{
 		if (visible_count(grid[i][3], grid[i][2], grid[i][1],
 				grid[i][0]) != constraints[12 + i])
 			return (0);
 		i++;
 	}
 	return (1);
+}
+
+int	check_constraints(int grid[4][4], int constraints[16])
+{
+	return (check_vertical(grid, constraints) && check_horizontal(grid,
+			constraints));
 }
